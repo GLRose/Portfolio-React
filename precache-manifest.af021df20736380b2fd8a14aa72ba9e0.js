8a1e817888f8b2fbebef5aba548d7783c6479a65
Garrett Rose <roseg3@winthrop.edu>
2022-07-09 13:59:40 -0400

    Updates

---
+++ precache-manifest.af021df20736380b2fd8a14aa72ba9e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff7d75aedff01d4ca11",
+    "url": "/react-gh-pages/static/css/main.3d550850.chunk.css"
+  },
+  {
+    "revision": "9ff7d75aedff01d4ca11",
+    "url": "/react-gh-pages/static/js/main.9ff7d75a.chunk.js"
+  },
+  {
+    "revision": "058e4258b82ecadfa8c9",
+    "url": "/react-gh-pages/static/js/1.058e4258.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "cc3eb36aa74e834aa9fe6a57a777f4f5",
+    "url": "/react-gh-pages/static/media/resume.cc3eb36a.pdf"
+  },
+  {
+    "revision": "9e4e3ec4acecef782e04aebc897fa681",
+    "url": "/react-gh-pages/static/media/Todo.9e4e3ec4.PNG"
+  },
+  {
+    "revision": "94e785bc2b787da80942ec6f71c3aeb4",
+    "url": "/react-gh-pages/static/media/git.94e785bc.PNG"
+  },
+  {
+    "revision": "6335649b3e48b672fde14094f740c6e8",
+    "url": "/react-gh-pages/static/media/logo.6335649b.png"
+  },
+  {
+    "revision": "16b47f5e86a09ee6aa78a07ebcdd8039",
+    "url": "/react-gh-pages/static/media/GR-Profile-blue.16b47f5e.png"
+  },
+  {
+    "revision": "e9b2593f4a634ae6fdbdf11b750153fa",
+    "url": "/react-gh-pages/static/media/customArrow.e9b2593f.svg"
+  },
+  {
+    "revision": "1ecb71efa452b02986c5935140b7142a",
+    "url": "/react-gh-pages/static/media/Background-site-no-logo.1ecb71ef.jpg"
+  },
+  {
+    "revision": "bb43ac2951fae67812e08e33a6045138",
+    "url": "/react-gh-pages/static/media/darkCustomArrow.bb43ac29.svg"
+  },
+  {
+    "revision": "f893986b35688465296122eb16848622",
+    "url": "/react-gh-pages/index.html"
+  }
+];
