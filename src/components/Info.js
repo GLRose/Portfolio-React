40548ea3182a01ca9ed964e48eae87fe8f4235e8
Garrett Rose <roseg3@winthrop.edu>
2022-07-12 15:13:03 -0400

    final

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Info.css";
-import Face from "../images/GR-Profile-blue.png";
+import Face from "../images/GR-PP.png";
 
 const About = () => {
   return (
     <div className="infoCard">
-      {/* <img className="Face" alt="Face" src={Face} /> */}
+      <img className="Face" alt="Face" src={Face} />
       <p className="hi">My name is Garrett Rose.</p>
       <p className="look">
-        I am a web developer creating and learning all the time.
+        I am a web developer creating and learning all of the time.
       </p>
     </div>
   );
