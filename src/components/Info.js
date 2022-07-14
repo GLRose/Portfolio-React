9977d27e238e099679c1a0609a1f6f243c962508
Garrett Rose <roseg3@winthrop.edu>
2022-07-14 16:00:12 -0400

    Made image a tag link to resume download

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Info.css";
 import Face from "../images/GR-PP.png";
+import Pdf from "../credentials/resume.pdf";
 
 const About = () => {
   return (
     <div className="infoCard">
-      <img className="Face" id="Face" alt="Face" src={Face} />
+      <a href={Pdf} download="resume.pdf" title="resume"><img className="Face" id="Face" alt="Face" src={Face} /></a>
       <p className="hi">My name is Garrett Rose.</p>
       <p className="look">
         I am a web developer creating and learning all of the time.
